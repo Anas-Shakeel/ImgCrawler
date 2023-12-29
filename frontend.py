d9af330c943676305f73451b178223b8e3b84ed1
Anas-Shakeel <srdranas456@gmail.com>
2023-12-30 02:52:17 +0500

    Added Basic Configs

--- frontend.py
+++ frontend.py
@@ -7,8 +7,15 @@ from backend import ImgPile
 
 
 class App(tk.Tk):
+    # program name
+    PROGRAM_NAME = "ImgCrawler"
+    
     def __init__(self):
         super().__init__()
 
-        # code for the frontend
-        ...
+        # * Root Configuration
+        self.title(self.PROGRAM_NAME)
+        self.geometry("1280x720+0+10")
+        self.minsize(520, 380)
+        self.state("zoomed")
+        self.option_add("*tearOff", tk.FALSE)
