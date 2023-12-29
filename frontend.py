dfbc9fd44754afd0f93e6306c9b544c15c956d50
Anas-Shakeel <srdranas@gmail.com>
2023-12-30 01:29:55 +0500

    Basic Template ready

--- frontend.py
+++ frontend.py
@@ -1,4 +1,14 @@
 """ 
 ### Frontend for ImgPile Scraper
 """
-...
+import tkinter as tk
+from tkinter import ttk
+from backend import ImgPile
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # Gui INIT
+        ...
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """ Main Starting point of the software """
 
-import frontend
-import backend
+from frontend import App
+
 
 def main():
-    # Code
-    ...
+    # * Code to execute the project
+    app = App()
+    app.mainloop()
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
