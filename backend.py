6d003120f5d75c5eefc3e4541ecbe48662f7eaa8
Anas-Shakeel <srdranas456@gmail.com>
2023-12-31 21:07:01 +0500

    Added Get Response method

--- backend.py
+++ backend.py
@@ -7,4 +7,12 @@ from imgpile import ImgPile
 
 class Backend:
     def __init__(self):
+        self.img_api = ImgPile()
         pass
+
+    def get_response(self, url):
+        """ 
+        ### Get Response
+        This method talks directly to the API and returns a response from it
+        """
+        return self.img_api.get(url)
