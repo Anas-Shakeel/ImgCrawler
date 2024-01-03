0822dbcc8d95a230eb9c130ea726af05dfe7c1bf
Anas-Shakeel <srdranas456@gmail.com>
2024-01-03 06:57:52 +0500

    Added To_bytes method

--- backend.py
+++ backend.py
@@ -16,3 +16,43 @@ class Backend:
         This method talks directly to the API and returns a response from it
         """
         return self.img_api.get(url)
+
+    def to_bytes(self, _size=137.5, _unit="MB"):
+        """
+        ### To Bytes
+        Converts other Data Storage units to bytes
+
+        ### Note
+        It also selects sizes like "5.2 mb" etc
+
+        ```
+        >> fm._to_bytes(_size="5.0 kb")
+        5120
+
+        >> fm._to_bytes(_size="5.0 mb")
+        5242880
+        ```
+        """
+        # Extract values
+        size = _size
+        unit = _unit
+
+        # size validation
+        if size <= 0:
+            return 0
+
+        # unit validation
+        if not (unit and unit.isalpha() and len(unit) in [1, 2]):
+            raise ValueError(f"'{unit}' is not a valid unit")
+
+        # ? Calculate bytes
+        # Unit Factors
+        factors = {
+            "B": 1,
+            "KB": 1024,
+            "MB": 1024 ** 2,
+            "GB": 1024 ** 3,
+        }
+
+        # ? Calculate bytes
+        return factors[unit] * size
